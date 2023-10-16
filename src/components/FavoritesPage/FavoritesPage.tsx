71c57e277e381784b7ddc6c022fbf9bba3f4c243
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-16 13:09:01 +0600

    add component CardFavorites

--- src/components/FavoritesPage/FavoritesPage.tsx
+++ src/components/FavoritesPage/FavoritesPage.tsx
@@ -9,7 +9,7 @@ function FavoritesPage(): JSX.Element {
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
               </a>
             </div>
             <nav className="header__nav">
@@ -61,7 +61,7 @@ function FavoritesPage(): JSX.Element {
                   </div>
                 </div>
                 <div className="favorites__places">
-                <CardFavorites />
+                  <CardFavorites />
                 </div>
               </li>
             </ul>
