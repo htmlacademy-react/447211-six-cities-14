c4e39b6b663a53e69b49639c0ca5a1307baf9174
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-18 16:39:02 +0600

    add component 404

---
+++ src/components/pages/page-404/page-404.tsx
@@ -0,0 +1,12 @@
+function Page404(): JSX.Element {
+  return (
+    <div className="page-404">
+      <div className="container">
+        <p>Page not found</p>
+        <a href="/">Back to Homepage</a>
+      </div>
+    </div>
+  )
+}
+
+export default Page404;
