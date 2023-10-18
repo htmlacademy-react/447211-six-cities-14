ea6f78fe82cc2ac4b1729c938e1098b52ba865ee
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-18 17:49:35 +0600

    styling page 404

--- src/components/pages/page-404/page-404.tsx
+++ src/components/pages/page-404/page-404.tsx
@@ -1,8 +1,10 @@
+import './page-404.css';
+
 function Page404(): JSX.Element {
   return (
     <div className="page-404">
       <div className="container">
-        <p>Page not found</p>
+        <h1>404. Page not found</h1>
         <a href="/">Back to Homepage</a>
       </div>
     </div>
