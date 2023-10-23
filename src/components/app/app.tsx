9252b36dafb5d2e620138253aa69f3e51de46458
Alexander Drach <alexander.drach.adci@gmail.com>
2023-10-23 16:16:40 +0600

    add private rout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Layout from '../layout';
 import MainPage from '../pages/main-page';
 import Login from '../pages/login-page';
@@ -6,22 +7,30 @@ import Favorites from '../pages/favorites-page';
 import Offer from '../pages/offer-page';
 import Page404 from '../pages/page-404';
 
+import { AuthorizationStatus } from '../../const/const';
+import PrivateRoute from '../private-route';
+
 
 type AppProps = {
   countOffers: number;
 }
 
-function App({countOffers}: AppProps): JSX.Element {
+function App({ countOffers }: AppProps): JSX.Element {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Layout />} />
-      <Route index element={<MainPage countOffers={countOffers} />}/>
-      <Route path='login' element={<Login />}/>
-      <Route path='favorites' element={<Favorites />}/>
-      <Route path='offer/:id' element={<Offer />}/>
-      <Route path='*' element={<Page404 />}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Layout />} />
+        <Route index element={<MainPage countOffers={countOffers} />} />
+        <Route path='login' element={<Login />} />
+        <Route path='favorites' element={
+          <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+            <Favorites />
+          </PrivateRoute>
+        }
+        />
+        <Route path='offer/:id' element={<Offer />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
